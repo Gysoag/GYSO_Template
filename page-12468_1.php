d79c93918f232a371f13e1fb25d5bb963a39b2f5
Jeffrey Widmer <jeffrey.widmer@gyso.ch>
2021-12-03 16:28:23 +0100

    -

--- page-12468_1.php
+++ page-12468_1.php
@@ -5,27 +5,42 @@
 
 <!-- || START -->
 <!-- ||@ Content Verkaufsladen / HTML -->
-<div class="grid-container-shop mt-30">
-    <div class="item-1"></div>
-    <div class="item-2">
-        <h3>Unsere Öffnungszeiten</h3>
-        <table class="openingHours-table">
-            <tr>
-            <td>Montag - Donnerstag</td>
-            <td>07.30-12.00 / 13.00-17.30</td>
-            </tr>
-            <tr>
-            <td>Freitag</td>
-            <td>07.30-12.00 / 13.00-17.00</td>
-            </tr>
-        </table>
+
+
+<img class="headImg" src="./kundenupload/content-management/img/gray.jpg" alt="">
+<h1 class="mt-30">Verkaufsladen / Beratung & Abholung</h1> 
+<h6 class="mt-5">An unseren zwei Standorten <b>Kloten (ZH)</b> und <b>Crissier (VD)</b> begrüssen wir Sie gerne in unseren Shops.</h6>
+<p class="mt-30">
+    Mit unseren GYSO-Shops bieten wir Ihnen einen praktischen Weg zum raschen Einkauf mit kompetenter Beratung. Die Bedienung durch unsere Fachleute erfolgt ohne Voranmeldung
+    oder Wartezeiten. Wir führen Qualitätsprodukte in den Kompetenzbereichen Kleben, Dichten, Schützen, Schleifen, Lackieren und Finish
+    für das Bau- und Automotivegewerbe.    
+</p>
+<div class="opc-row mt-30" data-op-align-items="center">
+   <div class="opc-col" data-op-col="12-6-6">
+        <img src="./kundenupload/content-management/img/gray.jpg" width="100%" height="400px">
     </div>
+   <div class="opc-col" data-op-col="12-6-6">
+       <h2>Zahlungsarten</h2>
+       <p class="mt-15">
+            Als registrierter Kunde können Sie die Waren (bei einem Warenwert ab CHF 30, exkl. MwSt.) bequem gegen Rechnung beziehen.
+            
+        </p>
+        <p class="mt-15">Für Kleinbezüge, welche unter diesem Warenwert liegen, stehen folgende Zahlungsarten zur Verfügung:</p>
+        <div class="mt-30">
+            <img  src="./kundenupload/content-management/img/gray.jpg" alt="" width="30%" height="100">
+            <img  src="./kundenupload/content-management/img/gray.jpg" alt="" width="30%" height="100">
+            <img  src="./kundenupload/content-management/img/gray.jpg" alt="" width="30%" height="100">
+        </div>
+   </div>
 </div>
-<h1 class="mt-30">Verkaufsladen / Beratung & Abholung</h1>
 
-
-
-
+<p class="mt-30">
+    Kleinmengenzuschlag: Bei Rechnungsstellung bis zu einem Warenwert von CHF 30 (exkl. MwSt.) erlauben wir uns für die Mehrkosten einen Kleinmengenzuschlag 
+    von CHF 20 zu belasten.
+</p>
+<p class="mt-15">
+    Wir freuen uns auf ihren Besuch.
+</p>
 
 
 
