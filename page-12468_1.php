b66f83037c10bd3ba36779c9ca653a357412ddc4
Jeffrey Widmer <jeffrey.widmer@gyso.ch>
2021-12-06 15:06:00 +0100

    Add Lieferservice

--- page-12468_1.php
+++ page-12468_1.php
@@ -7,41 +7,56 @@
 <!-- ||@ Content Verkaufsladen / HTML -->
 
 
-<img class="headImg" src="./kundenupload/content-management/img/gray.jpg" alt="">
-<h1 class="mt-30">Verkaufsladen / Beratung & Abholung</h1> 
-<h6 class="mt-5">An unseren zwei Standorten <b>Kloten (ZH)</b> und <b>Crissier (VD)</b> begrüssen wir Sie gerne in unseren Shops.</h6>
-<p class="mt-30">
-    Mit unseren GYSO-Shops bieten wir Ihnen einen praktischen Weg zum raschen Einkauf mit kompetenter Beratung. Die Bedienung durch unsere Fachleute erfolgt ohne Voranmeldung
-    oder Wartezeiten. Wir führen Qualitätsprodukte in den Kompetenzbereichen Kleben, Dichten, Schützen, Schleifen, Lackieren und Finish
-    für das Bau- und Automotivegewerbe.    
-</p>
-<div class="opc-row mt-30" data-op-align-items="center">
-   <div class="opc-col" data-op-col="12-6-6">
-        <img src="./kundenupload/content-management/img/gray.jpg" width="100%" height="400px">
-    </div>
-   <div class="opc-col" data-op-col="12-6-6">
-       <h2>Zahlungsarten</h2>
-       <p class="mt-15">
-            Als registrierter Kunde können Sie die Waren (bei einem Warenwert ab CHF 30, exkl. MwSt.) bequem gegen Rechnung beziehen.
-            
-        </p>
-        <p class="mt-15">Für Kleinbezüge, welche unter diesem Warenwert liegen, stehen folgende Zahlungsarten zur Verfügung:</p>
-        <div class="mt-30">
-            <img  src="./kundenupload/content-management/img/gray.jpg" alt="" width="30%" height="100">
-            <img  src="./kundenupload/content-management/img/gray.jpg" alt="" width="30%" height="100">
-            <img  src="./kundenupload/content-management/img/gray.jpg" alt="" width="30%" height="100">
+<!-- || Hauptbild -->
+<section class="section-block">
+    <img class="main-image" src="./kundenupload/content-management/img/gray.jpg" alt="">
+</section>
+
+<!-- || Titel und Beschreibung -->
+<section class="section-block">
+    <h1 class="mt-30">Verkaufsladen / Beratung & Abholung</h1> 
+    <h6 class="mt-5">An unseren zwei Standorten <b>Kloten (ZH)</b> und <b>Crissier (VD)</b> begrüssen wir Sie gerne in unseren Shops.</h6>
+    <p class="mt-15">
+        Mit unseren GYSO-Shops bieten wir Ihnen einen praktischen Weg zum raschen Einkauf mit kompetenter Beratung. Die Bedienung durch unsere Fachleute erfolgt ohne Voranmeldung
+        oder Wartezeiten. Wir führen Qualitätsprodukte in den Kompetenzbereichen Kleben, Dichten, Schützen, Schleifen, Lackieren und Finish
+        für das Bau- und Automotivegewerbe.    
+    </p>
+</section>
+
+<!-- || Zahlungsarten -->
+<section class="section-block mt-50">
+    <div class="opc-row" data-op-align-items="center">
+        <div class="opc-col opc-col-display" data-op-col="12-6-6">
+            <img src="./kundenupload/content-management/img/gray.jpg" width="100%" height="400px">
         </div>
-   </div>
-</div>
+        <div class="opc-col" data-op-col="12-6-6">
+            <h2>Zahlungsarten</h2>
+            <p>Als registrierter Kunde können Sie die Waren (bei einem Warenwert ab CHF 30, exkl. MwSt.) bequem gegen Rechnung beziehen.</p>
+            <p>Für Kleinbezüge, welche unter diesem Warenwert liegen, stehen folgende Zahlungsarten zur Verfügung:</p>
+            <div class="row mt-30">
+                <div class="column">
+                    <img class="zahlungsart-img" src="./kundenupload/content-management/img/verkaufsladen/barzahlung.png">
+                </div>
+                <div class="column">
+                    <img class="zahlungsart-img" src="./kundenupload/content-management/img/verkaufsladen/maestro.png">
+                </div>
+                <div class="column">
+                    <img class="zahlungsart-img" src="./kundenupload/content-management/img/verkaufsladen/postfinance.png">
+                </div>
+            </div>
+            <div class="mt-30">
+            <p>
+                Kleinmengenzuschlag: Bei Rechnungsstellung bis zu einem Warenwert von CHF 30 (exkl. MwSt.) erlauben wir uns für die Mehrkosten einen Kleinmengenzuschlag 
+                von CHF 20 zu belasten.
+            </p>
+            </div>
 
-<p class="mt-30">
-    Kleinmengenzuschlag: Bei Rechnungsstellung bis zu einem Warenwert von CHF 30 (exkl. MwSt.) erlauben wir uns für die Mehrkosten einen Kleinmengenzuschlag 
-    von CHF 20 zu belasten.
-</p>
-<p class="mt-15">
-    Wir freuen uns auf ihren Besuch.
-</p>
-
+            <p class="mt-15">
+                Wir freuen uns auf ihren Besuch.
+            </p>
+        </div>
+    </div>
+</section>
 
 
 <!-- || ENDE -->
---
+++ page-12471_1.php
@@ -0,0 +1,97 @@
+﻿<!-- Include Header -->
+<?php include './includes/header.php'; ?>
+<div id="page-12471">
+
+
+<!-- || START -->
+<!-- ||@ Content Verkaufsladen / HTML -->
+
+<!-- || Hauptbild -->
+<section class="section-block">
+    <img class="main-image" src="./kundenupload/content-management/img/gray.jpg" alt="">
+</section>
+
+<!-- || Titel und Beschreibung -->
+<section class="section-block">
+    <h1 class="mt-30">Lieferservice</h1> 
+    <h6 class="mt-5">Der rasche und kundengerechte Lieferservice ist uns sehr wichtig.</h6>
+</section>
+
+<!-- || Lieferservice -->
+<section class="section-block mt-30">
+    <div class="opc-row" data-op-align-items="">
+        <!-- Start Bild -->
+        <div class="opc-col" data-op-col="12-6-6">
+            <img class="img-right" src="./kundenupload/content-management/img/gray.jpg" width="100%" height="450px">
+        </div>
+        <!-- Ende Bild -->
+        <!-- Start Versandarten -->
+        <div class="opc-col" data-op-col="12-6-6">
+        <div class="row">
+            <div class="col">
+            <h2>Unsere Versandarten</h2>
+            <p class="mt-15">
+                Mit der sehr hohen Verfügbarkeit unserer Produkte sowie der modernen und effizienten Logistik, 
+                garantieren wir Ihnen die Auslieferung Ihrer Bestellung innert kürzester Zeit.   
+            </p>
+            <div class="tabs mt-15">
+            <div class="tab">
+                <input type="radio" id="rd1" name="rd">
+                <label class="tab-label" for="rd1">Normalversand</label>
+                <div class="tab-content">
+                    Bei Bestellungen bis 16.00 Uhr erfolgt der Versand in der Regel am selben Tag und die Ware wird Ihnen am nächsten Tag oder spätestens am Folgetag zugestellt.
+                </div>
+            </div>
+            <div class="tab"></div>
+                <input type="radio" id="rd2" name="rd">
+                <label class="tab-label" for="rd2">Spätversand</label>
+                <div class="tab-content">
+                    Dringende Postsendungen werden bis 17.30 Uhr (Freitag bis 17.00 Uhr) angenommen und noch am selben Tag zum Versand gebracht. Die Zustellung erfolgt in der Regel am Folgetag.
+                </div>
+            </div>
+            <div class="tab">
+                <input type="radio" id="rd3" name="rd">
+                <label class="tab-label" for="rd3">GYSO Tour</label>
+                <div class="tab-content">
+                    Im erweiterten Einzugsgebiet unserer Standorte Kloten (ZH) und Crissier (VD) bieten wir unseren Kunden einen hauseigenen Auslieferdienst an. 
+                </div>
+            </div>
+            <div class="tab">
+                <input type="radio" id="rd4" name="rd" open>
+                <label class="tab-label" for="rd4">Abholen im Verkaufsladen</label>
+                <div class="tab-content">
+                    In unseren Verkaufsläden können Sie das benötigte Material während den Öffnungszeiten ohne Voranmeldung und Wartezeit abholen.
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <!-- Ende Versandarten -->
+    </div>
+</section>
+
+<!-- Start Sonderfälle -->
+<section class="section-block mt-30">
+<h4>In folgenden Sonderfälle kann sich die Lieferzeit verzögern</h4>
+<ul>
+  <li>Güter welche aufgrund von Gewicht, Sperrigkeit oder Inhaltsstoffe per Camion (Fracht) transportiert werden müssen.</li>
+  <li>Bestellungen mit konfektionierter Ware: Zuschnitt und Abfüllungen (Service-Center).</li>
+  <li>Bestellungen mit erhöhtem Abklärungsbedarf.</li>
+</ul>
+<p>
+    Der rasche und kundengerechte Lieferservice ist uns sehr wichtig. Melden Sie Ihre individuellen Lieferwünsche bei unserem Verkauf Innendienst und wir setzen alles daran 
+    diese zu erfüllen.
+</p>
+</section>
+<!-- Ende Sonderfälle -->
+
+
+
+
+
+<!-- || ENDE -->
+
+
+</div>
+<!-- Include Footer -->
+<?php include './includes/footer.php'; ?>
